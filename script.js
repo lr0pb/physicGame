fc62e52204b079c654a0242d38a173345caed864
lr0pb <hello.lr0pb@gmail.com>
2020-03-19 11:25:56 +0200

    Test sw

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ if ('serviceWorker' in navigator) {
         show(document.querySelector('#updateBlock'));
       };
     });
+    fetch('https://fonts.googleapis.com/css?family=Montserrat:600,700,800&display=swap&subset=cyrillic');
   })
 };
 

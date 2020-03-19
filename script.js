566cfae1620c7e2188ec86737ff804329ac1ea66
lr0pb <hello.lr0pb@gmail.com>
2020-03-19 14:26:17 +0200

    Fix font connection

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ if ('serviceWorker' in navigator) {
         show(document.querySelector('#updateBlock'));
       };
     });
-    document.querySelector('#font').setAttribute('href','https://fonts.googleapis.com/css?family=Montserrat:600,700,800&display=swap&subset=cyrillic');
+    fetch('https://fonts.googleapis.com/css?family=Montserrat:600,700,800&display=swap');
   })
 };
 
